fb490078c2df8b53f37f6ef0bf68d30fda820720
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-01-02 11:48:33 +0000

    Fix go vet warning
    Passing the handler rather than the address copied the sync.RWMutex
    
    Doesn't matter in this case, but 'go vet' was getting sad
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- testing/broker_tester_test.go
+++ testing/broker_tester_test.go
@@ -16,77 +16,76 @@ var _ = Describe("Broker Tester", func() {
 		var brokerAPI fakes.FakeHandler
 		var brokerTester testing.BrokerTester
 
-
-		BeforeEach(func(){
+		BeforeEach(func() {
 			credentials = brokerapi.BrokerCredentials{Username: "user", Password: "password"}
 			brokerAPI = fakes.FakeHandler{}
 			brokerTester = testing.New(credentials, &brokerAPI)
 		})
 
-		It("'Services' makes a GET request to the right place", func(){
+		It("'Services' makes a GET request to the right place", func() {
 			brokerTester.Services()
 
 			Expect(brokerAPI.ServeHTTPCallCount()).To(Equal(1))
-			Expect(getRequestMethod(brokerAPI)).To(Equal("GET"))
-			Expect(getRequestPath(brokerAPI)).To(Equal("/v2/catalog"))
+			Expect(getRequestMethod(&brokerAPI)).To(Equal("GET"))
+			Expect(getRequestPath(&brokerAPI)).To(Equal("/v2/catalog"))
 		})
 
-		It("'Provision' makes a PUT request to the right place", func(){
+		It("'Provision' makes a PUT request to the right place", func() {
 			brokerTester.Provision("instance_id", testing.RequestBody{}, false)
 
 			Expect(brokerAPI.ServeHTTPCallCount()).To(Equal(1))
-			Expect(getRequestMethod(brokerAPI)).To(Equal("PUT"))
-			Expect(getRequestPath(brokerAPI)).To(Equal("/v2/service_instances/instance_id"))
+			Expect(getRequestMethod(&brokerAPI)).To(Equal("PUT"))
+			Expect(getRequestPath(&brokerAPI)).To(Equal("/v2/service_instances/instance_id"))
 		})
 
-		It("'Deprovision' makes a DELETE request to the right place", func(){
+		It("'Deprovision' makes a DELETE request to the right place", func() {
 			brokerTester.Deprovision("instance_id", "service_id", "plan_id", false)
 
 			Expect(brokerAPI.ServeHTTPCallCount()).To(Equal(1))
-			Expect(getRequestMethod(brokerAPI)).To(Equal("DELETE"))
-			Expect(getRequestPath(brokerAPI)).To(Equal("/v2/service_instances/instance_id"))
+			Expect(getRequestMethod(&brokerAPI)).To(Equal("DELETE"))
+			Expect(getRequestPath(&brokerAPI)).To(Equal("/v2/service_instances/instance_id"))
 		})
 
-		It("'Bind' makes a PUT request to the right place", func(){
+		It("'Bind' makes a PUT request to the right place", func() {
 			brokerTester.Bind("instance_id", "binding_id", testing.RequestBody{}, false)
 
 			Expect(brokerAPI.ServeHTTPCallCount()).To(Equal(1))
-			Expect(getRequestMethod(brokerAPI)).To(Equal("PUT"))
-			Expect(getRequestPath(brokerAPI)).To(Equal("/v2/service_instances/instance_id/service_bindings/binding_id"))
+			Expect(getRequestMethod(&brokerAPI)).To(Equal("PUT"))
+			Expect(getRequestPath(&brokerAPI)).To(Equal("/v2/service_instances/instance_id/service_bindings/binding_id"))
 		})
 
-		It("'Unbind' makes a DELETE request to the right place", func(){
+		It("'Unbind' makes a DELETE request to the right place", func() {
 			brokerTester.Unbind("instance_id", "service_id", "plan_id", "binding_id", false)
 
 			Expect(brokerAPI.ServeHTTPCallCount()).To(Equal(1))
-			Expect(getRequestMethod(brokerAPI)).To(Equal("DELETE"))
-			Expect(getRequestPath(brokerAPI)).To(Equal("/v2/service_instances/instance_id/service_bindings/binding_id"))
+			Expect(getRequestMethod(&brokerAPI)).To(Equal("DELETE"))
+			Expect(getRequestPath(&brokerAPI)).To(Equal("/v2/service_instances/instance_id/service_bindings/binding_id"))
 		})
 
-		It("'Update' makes a PATCH request to the right place", func(){
+		It("'Update' makes a PATCH request to the right place", func() {
 			brokerTester.Update("instance_id", testing.RequestBody{}, false)
 
 			Expect(brokerAPI.ServeHTTPCallCount()).To(Equal(1))
-			Expect(getRequestMethod(brokerAPI)).To(Equal("PATCH"))
-			Expect(getRequestPath(brokerAPI)).To(Equal("/v2/service_instances/instance_id"))
+			Expect(getRequestMethod(&brokerAPI)).To(Equal("PATCH"))
+			Expect(getRequestPath(&brokerAPI)).To(Equal("/v2/service_instances/instance_id"))
 		})
 
-		It("'LastOperation' makes a GET request to the right place", func(){
+		It("'LastOperation' makes a GET request to the right place", func() {
 			brokerTester.LastOperation("instance_id", "service_id", "plan_id", "operation")
 
 			Expect(brokerAPI.ServeHTTPCallCount()).To(Equal(1))
-			Expect(getRequestMethod(brokerAPI)).To(Equal("GET"))
-			Expect(getRequestPath(brokerAPI)).To(Equal("/v2/service_instances/instance_id/last_operation"))
+			Expect(getRequestMethod(&brokerAPI)).To(Equal("GET"))
+			Expect(getRequestPath(&brokerAPI)).To(Equal("/v2/service_instances/instance_id/last_operation"))
 		})
 	})
 })
 
-func getRequestMethod(handler fakes.FakeHandler) string {
+func getRequestMethod(handler *fakes.FakeHandler) string {
 	_, req := handler.ServeHTTPArgsForCall(0)
 	return req.Method
 }
 
-func getRequestPath(handler fakes.FakeHandler) string {
+func getRequestPath(handler *fakes.FakeHandler) string {
 	_, req := handler.ServeHTTPArgsForCall(0)
 	return req.URL.Path
 }

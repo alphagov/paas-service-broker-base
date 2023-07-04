60aa022cb990b4c9a87d213dd059316a48a62410
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2023-07-04 09:45:38 +0100

    Implement support for TLS configuration
    - enable a 'tls' config block to be passed in, with `certificate`,
      `private_key` and `ca` fields.
    - if `tls` is provided, validate that all three fields are not `''`
    - Add a helper method to create a `tls.Config` object, containing:
      - the certificate:private key pair
      - the ca chain
      - sane defaults for:
        - minimum TLS version
        - cipher suites
        - curve preferences
    
    These can then be used by brokers to create a TLS listener instead
    of a http one.

--- broker/config.go
+++ broker/config.go
@@ -1,6 +1,8 @@
 package broker
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -15,7 +17,9 @@ import (
 )
 
 const (
-	DefaultPort     = "3000"
+	DefaultPort = "3000"
+	DefaultHost = "0.0.0.0"
+
 	DefaultLogLevel = "debug"
 )
 
@@ -36,12 +40,13 @@ func NewConfig(source io.Reader) (Config, error) {
 	if err = json.Unmarshal(bytes, &api); err != nil {
 		return config, err
 	}
-	if api.Port == "" {
-		api.Port = "3000"
+
+	api.fillDefaults()
+	err = api.validate()
+	if err != nil {
+		return config, err
 	}
-	if api.LogLevel == "" {
-		api.LogLevel = "debug"
-	}
+
 	api.LagerLogLevel, err = api.ConvertLogLevel()
 	if err != nil {
 		return config, err
@@ -64,12 +69,6 @@ func NewConfig(source io.Reader) (Config, error) {
 }
 
 func (c Config) Validate() error {
-	if c.API.BasicAuthUsername == "" {
-		return fmt.Errorf("Config error: basic auth username required")
-	}
-	if c.API.BasicAuthPassword == "" {
-		return fmt.Errorf("Config error: basic auth password required")
-	}
 	if reflect.DeepEqual(c.Catalog, Catalog{}) {
 		return fmt.Errorf("Config error: catalog required")
 	}
@@ -81,25 +80,71 @@ func (c Config) Validate() error {
 			return fmt.Errorf("Config error: no plans found for service %s", service.Name)
 		}
 	}
-	if c.API.Locket != nil {
-		if c.API.Locket.Address == "" {
-			return fmt.Errorf("Config error: locket address required")
-		}
+	return nil
+}
+
+type TLS struct {
+	Certificate string `json:"certificate"`
+	PrivateKey  string `json:"private_key"`
+	CA          string `json:"ca"`
+}
+
+func (t *TLS) validate() error {
+	if t.Certificate == "" {
+		return fmt.Errorf("Config error: TLS certificate required")
+	}
+	if t.PrivateKey == "" {
+		return fmt.Errorf("Config error: TLS private key required")
+	}
+	if t.CA == "" {
+		return fmt.Errorf("Config error: TLS CA required")
 	}
 	return nil
 }
 
-type API struct {
-	BasicAuthUsername     string `json:"basic_auth_username"`
-	BasicAuthPassword     string `json:"basic_auth_password"`
-	Port                  string `json:"port"`
-	LogLevel              string `json:"log_level"`
-	LagerLogLevel         lager.LogLevel
-	Locket                *LocketConfig `json:"locket"`
-	ContextTimeoutSeconds int           `json:"context_timeout_seconds"`
+func (t *TLS) Config() (*tls.Config, error) {
+	certificate, err := tls.X509KeyPair([]byte(t.Certificate), []byte(t.PrivateKey))
+	if err != nil {
+		return nil, err
+	}
+
+	caPool := x509.NewCertPool()
+	caPool.AddCert(&x509.Certificate{
+		Raw: []byte(t.CA),
+	})
+	return &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+		RootCAs:      caPool,
+
+		MinVersion:       tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}, nil
 }
 
-func (api API) ConvertLogLevel() (lager.LogLevel, error) {
+type API struct {
+	BasicAuthUsername string `json:"basic_auth_username"`
+	BasicAuthPassword string `json:"basic_auth_password"`
+	Port              string `json:"port"`
+	Host              string `json:"host"`
+	TLS               *TLS   `json:"tls"`
+
+	LogLevel      string `json:"log_level"`
+	LagerLogLevel lager.LogLevel
+
+	Locket *LocketConfig `json:"locket"`
+
+	ContextTimeoutSeconds int `json:"context_timeout_seconds"`
+}
+
+func (api *API) ConvertLogLevel() (lager.LogLevel, error) {
 	logLevels := map[string]lager.LogLevel{
 		"DEBUG": lager.DEBUG,
 		"INFO":  lager.INFO,
@@ -113,11 +158,47 @@ func (api API) ConvertLogLevel() (lager.LogLevel, error) {
 	return logLevel, nil
 }
 
-func (api API) ContextTimeout() time.Duration {
+func (api *API) ContextTimeout() time.Duration {
 	if api.ContextTimeoutSeconds == 0 {
 		return DefaultContextTimeout
 	}
 	return time.Duration(api.ContextTimeoutSeconds) * time.Second
+}
+
+func (api *API) validate() error {
+	if api.BasicAuthUsername == "" {
+		return fmt.Errorf("Config error: basic auth username required")
+	}
+	if api.BasicAuthPassword == "" {
+		return fmt.Errorf("Config error: basic auth password required")
+	}
+
+	if api.Locket != nil {
+		if api.Locket.Address == "" {
+			return fmt.Errorf("Config error: locket address required")
+		}
+	}
+
+	if api.TLS != nil {
+		return api.TLS.validate()
+	}
+	return nil
+}
+
+func (api *API) TLSEnabled() bool {
+	return api.TLS != nil
+}
+
+func (api *API) fillDefaults() {
+	if api.Port == "" {
+		api.Port = DefaultPort
+	}
+	if api.Host == "" {
+		api.Host = DefaultHost
+	}
+	if api.LogLevel == "" {
+		api.LogLevel = DefaultLogLevel
+	}
 }
 
 type Catalog struct {
